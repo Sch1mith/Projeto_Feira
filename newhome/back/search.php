8beca401bfd965f48740d4abea900d4dd5c97ac4
Sch1mith <165226331+Sch1mith@users.noreply.github.com>
2024-11-20 16:22:12 -0300

    Add files via upload

---
+++ newhome/back/search.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'DBConnection.php';
+
+if (isset($_GET['search'])) {
+    $searchQuery = $_GET['search'];
+
+    // Establish database connection
+    $conn = DBConnection::getConnection();
+    $sql = "SELECT * FROM lixo WHERE nome LIKE :searchQuery";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue(':searchQuery', '%' . $searchQuery . '%');
+
+    $stmt->execute();
+
+    // Fetch all results
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($results) {
+        $tipo = $results[0]['tipo'];
+        header("Location: ../main/html/conteudo.html?tab=" . urlencode($tipo));
+        exit;
+    } else {
+        header("Location: ../main/html/conteudo.html?tab=default");
+        exit;
+    }
+}
+?>
