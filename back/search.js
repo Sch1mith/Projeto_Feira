db19cc8760334b0e9f7d62ed97d22dcc561cc555
Sch1mith <165226331+Sch1mith@users.noreply.github.com>
2024-11-29 11:02:04 -0300

    Add files via upload

---
+++ back/search.js
@@ -0,0 +1,24 @@
+const valorProcurado = localStorage.getItem('dataValor');
+
+if (valorProcurado) {
+    const button = document.querySelector(`button[data-valor="${valorProcurado}"]`);
+    
+    if (button) {
+        const buttonAtivo = document.querySelector('button.active');
+        const tabAtiva = document.querySelector('.tab-pane.active');
+        if (buttonAtivo) {
+            buttonAtivo.classList.remove('active');
+        }
+        if (tabAtiva) {
+            tabAtiva.classList.remove('active');
+        }
+        button.classList.add('active');
+        const tab = document.querySelector(`#${button.getAttribute('data-valor')}`);
+        if (tab) {
+            tab.classList.add('active');
+        }
+        localStorage.removeItem('dataValor');
+    }
+} else {
+    console.error("Nenhum valor encontrado no LocalStorage.");
+}
---
+++ back/search.php
@@ -0,0 +1,52 @@
+<?php
+$host = 'localhost';
+$dbname = 'projeto';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if (isset($_POST['termo'])) {
+        $termo = $_POST['termo'];
+
+        $stmt = $pdo->prepare("SELECT tipo FROM lixo WHERE nome = :termo");
+        $stmt->execute([':termo' => $termo]);
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($resultado) {
+            $tipo = $resultado['tipo'];
+        } else {
+            $tipo = '';
+        }
+    }
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const valor = <?php echo json_encode($tipo); ?>;
+
+            if (valor) {
+                localStorage.setItem('dataValor', valor); 
+            } else {
+                localStorage.setItem('dataValor', "home");
+            }
+            window.location.href = "../main/Index.html"; 
+        });
+    </script>
+</body>
+
+</html>
